179d6bf259537f43ca02c5e76e8d3960f969f48d
nyeroni <nir2y@naver.com>
2024-01-07 13:54:23 +0900

    Update: Team과 연관관계 설정 및 생성자 생성

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -14,6 +14,25 @@ public class Member {
     private Long id;
     private String username;
     private int age;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+    public Member(String username) {
+        this(username, 0);
+    }
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
 }
