17f7f9e86c1b8b2f2556a883c4ca20126abd1573
nyeroni <nir2y@naver.com>
2024-01-08 17:51:34 +0900

    Update: 생성자, 수정자 코드 추가

--- src/main/java/study/datajpa/entity/BaseEntity.java
+++ src/main/java/study/datajpa/entity/BaseEntity.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -21,4 +23,11 @@ public class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
 }
