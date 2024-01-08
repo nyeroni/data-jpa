f1337a44e4de572f463f334f40e2f2dc573f7e57
nyeroni <nir2y@naver.com>
2024-01-08 17:19:36 +0900

    Feat: 사용자 정의 인터페이스 구현 클래스

---
+++ src/main/java/study/datajpa/repository/MemberRepositoryCustomImpl.java
@@ -0,0 +1,18 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+
+import java.lang.reflect.Member;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class MemberRepositoryCustomImpl implements MemberRepositoryCustom{
+
+    private final EntityManager em;
+
+    @Override
+    public List<Member> findMemberCustom(){
+        return em.createQuery("select m from Member m").getResultList();
+    }
+}
